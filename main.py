4c459242183ca37bcfc511bc3014f424c9000c9a
Vasyl Pavlyuchenko <vasyl.pavlyuchenko@gmail.com>
2024-11-18 17:21:57 +0200

    found exact solution

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import numpy as np
 import sympy as sp
 
-m = 10 # к-сть вузлів інтерполяції
-n = 3 # степінь полінома
-
 def chebishev_zeros(a, b):
     zeros = []
     for k in range (n):
         zeros.append((a + b) / 2 + (b - a) / 2 * np.cos((2 * k + 1) * np.pi / (2 * n))) # нулі полінома Чебишева
     return zeros
 
-print(chebishev_zeros(-1, 1))
+x = sp.Symbol('x')
+f = np.e ** x - 2 * (x - 1) ** 2
+exact_solution = sp.nsolve(f, x, 1)
+print(f"Exact solution: {exact_solution}")
+
+m = 10 # к-сть вузлів інтерполяції
+n = m - 1 # степінь полінома
