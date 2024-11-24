4272312cbb7864ec3f155753e4f6959e430a75bf
Vasyl Pavlyuchenko <vasyl.pavlyuchenko@gmail.com>
2024-11-24 19:37:45 +0200

    added plots

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sympy as sp
+import matplotlib.pyplot as plt
 
 def is_valid_interval(a, b, solution):
     solution_in_interval = False
@@ -93,7 +94,7 @@ exact_solution = sp.nsolve(f, x, 1)
 print(f"Точний розв'язок: {exact_solution}")
 
 # Дослідження інтервалу
-a = 0
+a = -1
 b = 1
 
 if not is_valid_interval(a, b, exact_solution):
@@ -114,3 +115,41 @@ print(f"Пряма інтерполяція: {forward_solution}")
 # Зворотня інтерполяція
 backward_solution, backward_polinom = backward_intepolation(nodes)
 print(f"Зворотня інтерполяція: {backward_solution}")
+
+# Графічне зображення функції та поліномів
+x_vals = np.linspace(-4, 4, 100)
+y_vals = f_func(x_vals)
+
+# Обмеження розмірів
+plt.figure(figsize=(10, 5))
+plt.xlim(-2, 2)
+plt.ylim(-10, 10)
+
+# Вісі координат
+plt.axhline(0, color='black', lw=1)
+plt.axvline(0, color='black', lw=1)
+
+# Графік функції
+plt.plot(x_vals, y_vals, label='f(x)', color='blue')
+
+# Графік прямої інтерполяції
+forward_polinom_func = sp.lambdify(x, forward_polinom)
+forward_polinom_vals = forward_polinom_func(x_vals)
+plt.plot(x_vals, forward_polinom_vals, label='Пряма інтерполяція', color='orange', linestyle='--')
+
+# Графік зворотної інтерполяції
+backward_polinom_func = sp.lambdify(sp.Symbol('y'), backward_polinom)
+backward_polinom_vals = backward_polinom_func(x_vals)
+plt.plot(x_vals, backward_polinom_vals, label='Зворотня інтерполяція', color='green', linestyle='--')
+
+# Точки інтерполяції
+plt.scatter(nodes, f_func(nodes), color='black', label='Вузли інтерполяції')
+
+# Розв'язки
+plt.scatter(forward_solution, 0, color='red', label='Корінь прямої інтерполяції')
+plt.scatter(backward_solution, 0, color='purple', label='Корінь зворотної інтерполяції')
+plt.scatter(exact_solution, 0, color='brown', label='Точний розв\'язок')
+
+plt.legend()
+plt.grid()
+plt.show()
